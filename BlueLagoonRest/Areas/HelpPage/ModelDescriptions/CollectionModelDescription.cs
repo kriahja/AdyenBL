905cd25b866db37fd29ae38c02ee06c68b7e428b
Kristján Aron Hjartarson <kristjan.aron.hjartarson@bluelagoon.is>
2018-05-23 11:42:48 +0000

    clean master

--- BlueLagoonRest/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace BlueLagoonRest.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
